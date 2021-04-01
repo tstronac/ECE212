e9db797cf6ba94c94f0572eb31cb2f48fb618f42
tstronac <60280859+tstronac@users.noreply.github.com>
2021-04-01 11:00:41 -0600

    Update Lab3c.s

--- Lab3c.s
+++ Lab3c.s
@@ -9,24 +9,30 @@
 .text
 Display:
 /*-----------------Students write their subroutine here--------------------*/
-
+/* load the display message*/
 ldr r0, =display1
 bl printf
 
+/* pop the array length */
 pop {r4}
 mov r0, r4
 bl value
 bl cr
 
-POP {r5}
+/*Push the return address to the stack*/
 push {lr}
 
+/*display the second error message*/
 ldr r0, =display2
 bl printf
 bl cr
 
+/*clear r6,r7 to zero*/
 mov r6, #0
 mov r7, #0
+
+/*a loop that continues for the length of the array, incrementing the address by r7*/
+/*if the coutner reaches the end,branch to the end message*/
 print_arr:
 ldr r0, [r5, r7]
 bl value
@@ -37,16 +43,14 @@ cmp r6, r4
 beq end
 b print_arr
 
+/*print last message*/
 end:
 ldr r0, =display3
 bl printf
 bl cr
 
-pop {r6}
-push {r5}
-push {r4}
+pop {PC}
 
-BX lr
 .data
 display1:
 .string "The Number of Entries was: 	"
